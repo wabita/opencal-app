90f08eef2a9659157b19d58da170696f25d1d19d
yokoyama asa <sayosayo30g@gmail.com>
2026-03-11 14:09:50 +0900

    フォルダ構成の作成

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # 追加
+
+app = FastAPI()
+
+# これを追加：フロントエンド（3000番）からのアクセスを許可する
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"], # 全てのメソッド（GET, POSTなど）を許可
+    allow_headers=["*"], # 全てのヘッダーを許可
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, your backend is working with CORS!"}
